2a5e4e426a99678aa789368cca2e2d6d0142b92c
ngosang <ngosang@hotmail.es>
2023-02-02 19:25:34 +0100

    Exit if the repository password is not configured

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ export PASSWORD_FILE="/tmp/restic_passwd"
 if [ -z "${RESTIC_REPO_PASSWORD}" ]; then
   if [ -z "${RESTIC_REPO_PASSWORD_FILE}" ]; then
     echo "You have to define one of these environment variables: RESTIC_REPO_PASSWORD or RESTIC_REPO_PASSWORD_FILE"
+    exit 1
   else
     cp "${RESTIC_REPO_PASSWORD_FILE}" "${PASSWORD_FILE}"
   fi
