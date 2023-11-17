2d3d2012b641e5038186698d92b98d20ea2991f7
Igor Cvetkovic <igor.cvetkovic@modoolar.com>
2023-11-17 15:02:43 +0100

    [IMPL] Retention policy monitoring

--- restic-exporter.py
+++ restic-exporter.py
@@ -9,9 +9,10 @@ import re
 import subprocess
 import sys
 import traceback
+import calendar
 
 from dateutil import parser
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily, REGISTRY
@@ -20,11 +21,13 @@ from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily, REGIS
 class ResticCollector(object):
     def __init__(self, repository, password_file_path, storage_boxes, **kwargs):
         # required stuff - repository, path to password file,
-        # list of storageboxes where repository for hostis placed
+        # list of storageboxes where repository for host is placed
         self.repository = repository
         self.password_file_path = password_file_path
         self.storage_boxes = storage_boxes
         self.repository_name = kwargs.get('repository_name', 'default')
+        self.retention_policy = kwargs.get('retention_policy', None)
+        self.backup_times = kwargs.get('backup_times', [3])
 
         # exporter config
         self.exit_on_error = kwargs.get('exit_on_error', False)
@@ -72,6 +75,10 @@ class ResticCollector(object):
             "repository",
         ]
 
+        retention_policy_labels = [
+            'policy_name',
+        ]
+
         common_label_names = [
             "client_hostname",
             "client_username",
@@ -92,6 +99,7 @@ class ResticCollector(object):
         ]
 
         all_snapshot_labels.extend(common_host_labels)
+        retention_policy_labels.extend(common_host_labels)
 
         # add common host labels to per_tag_label_names and common_label_names
         common_label_names.extend(common_host_labels)
@@ -155,6 +163,25 @@ class ResticCollector(object):
             labels=all_snapshot_labels,
         )
 
+        snapshots_by_retention_policy = GaugeMetricFamily(
+            "restic_snapshots_by_retention_policy",
+            "Number of snapshots by retention policy",
+            labels=retention_policy_labels,
+        )
+
+        # this metrics is present only because we cannot use values from labels as grafana max values
+        snapshot_retention_policy = GaugeMetricFamily(
+            "restic_snapshot_retention_policy",
+            "Retention policy for type of snapshot",
+            labels=retention_policy_labels,
+        )
+
+        snapshots_by_retention_policy_state = GaugeMetricFamily(
+            "restic_snapshots_by_retention_policy_state",
+            "State of retention policy for type of snapshot",
+            labels=retention_policy_labels,
+        )
+
         for metric in self.metrics:
             common_host_label_values = [
                 metric["storagebox"],
@@ -173,6 +200,17 @@ class ResticCollector(object):
                 by_type_label_values.extend(common_host_label_values)
 
                 backup_snapshots_by_type.add_metric(by_type_label_values, count)
+
+            for policy_name, policy_data in metric["snapshots_by_policy"].items():
+                by_policy_label_values = [
+                    policy_name,
+                ]
+
+                by_policy_label_values.extend(common_host_label_values)
+
+                snapshot_retention_policy.add_metric(by_policy_label_values, policy_data['policy'])
+                snapshots_by_retention_policy.add_metric(by_policy_label_values, policy_data['found'])
+                snapshots_by_retention_policy_state.add_metric(by_policy_label_values, int(policy_data['expected'] <= policy_data['found']))
 
             for snapshot_id, snapshot in metric["snapshots_stats"].items():
                 snapshot_label_values = [
@@ -217,6 +255,9 @@ class ResticCollector(object):
             yield backup_snapshots_by_type
             yield snapshot_size
             yield snapshot_files_count
+            yield snapshot_retention_policy
+            yield snapshots_by_retention_policy
+            yield snapshots_by_retention_policy_state
             yield scrape_duration_seconds
 
     def refresh(self, exit_on_error=False):
@@ -273,10 +314,26 @@ class ResticCollector(object):
     def _parse_metrics(self, storagebox: str = None):
         start_time = time.time()
 
+        today = datetime.now().astimezone()
+
         all_snapshots = self.get_snapshots(storagebox)
 
         snap_total_counter = {}
         snap_by_type_total_counter = {}
+        snap_by_type_retention_policy = {
+            'manual': {'policy': self.retention_policy.get('manual', 0), 'expected': 0, 'found': 0},
+            'update': {'policy': self.retention_policy.get('update', 0), 'expected': 0, 'found': 0},
+            'hourly': {'policy': self.retention_policy.get('hourly', 0), 'expected': 0, 'found': 0},
+            'daily': {'policy': self.retention_policy.get('daily', 0), 'expected': 0, 'found': 0},
+            'weekly': {'policy': self.retention_policy.get('weekly', 0), 'expected': 0, 'found': 0},
+            'monthly': {'policy': self.retention_policy.get('monthly', 0), 'expected': 0, 'found': 0},
+            'yearly': {'policy': self.retention_policy.get('yearly', 0), 'expected': 0, 'found': 0},
+        }
+
+        # to calculate snapshots_by_retention_policy_state, we need the oldest snapshot timestamp
+        # so we can precisely calculate when the backups have started, so we can compare the counts
+        oldest_snapshot = today.timestamp()
+
         snapshot_stats = {}
         for snap in all_snapshots:
             # get total number of matching snapshots
@@ -301,6 +358,141 @@ class ResticCollector(object):
                 "total_file_count": snap_stats.get('total_file_count', 0),
             }
 
+            if oldest_snapshot > float(snapshot_stats[snap["id"]]["snapshot_timestamp"]):
+                oldest_snapshot = float(snapshot_stats[snap["id"]]["snapshot_timestamp"])
+
+        # calculate possible number of backups for each retention policy, based on oldest_snapshot value
+        # this is required to determine if the retention policy is fulfilled or not
+        # for daily backups, we need to calculate the number of days between oldest_snapshot and today
+        # for weekly backups, we need to calculate the number of weeks between oldest_snapshot and today
+        # for monthly backups, we need to calculate the number of months between oldest_snapshot and today
+        # for yearly backups, we need to calculate the number of years between oldest_snapshot, and today
+        # we will not alert for manual and update snapshots, since they aren't automated
+        max_daily_snaps = round((today.timestamp() - oldest_snapshot) / timedelta(days=1).total_seconds())
+        if today.timestamp() - oldest_snapshot >= 7:
+            max_weekly_snaps = round((today.timestamp() - oldest_snapshot) / timedelta(weeks=1).total_seconds())
+        else:
+            max_weekly_snaps = 0
+
+        if today.timestamp() - oldest_snapshot >= 30:
+            max_monthly_snaps = round((today.timestamp() - oldest_snapshot) / timedelta(days=30).total_seconds())
+        else:
+            max_monthly_snaps = 0
+
+        if today.timestamp() - oldest_snapshot >= 365:
+            max_yearly_snaps = round((today.timestamp() - oldest_snapshot) / timedelta(days=365).total_seconds())
+        else:
+            max_yearly_snaps = 0
+
+        snap_by_type_retention_policy['hourly']['expected'] = 0
+        snap_by_type_retention_policy['daily']['expected'] = min(max_daily_snaps, self.retention_policy.get('daily', 0))
+        snap_by_type_retention_policy['weekly']['expected'] = min(max_weekly_snaps, self.retention_policy.get('weekly', 0))
+        snap_by_type_retention_policy['monthly']['expected'] = min(max_monthly_snaps, self.retention_policy.get('monthly', 0))
+        snap_by_type_retention_policy['yearly']['expected'] = min(max_yearly_snaps, self.retention_policy.get('yearly', 0))
+
+        # calculate expected number of hourly backups, based on time passed between start of the day and now
+        # if hour is in self.backup_times, we'll count it as expected backup
+        for hour in range(0, today.hour+1):
+            if hour in self.backup_times:
+                snap_by_type_retention_policy['hourly']['expected'] += 1
+
+        for tag, count in snap_by_type_total_counter.items():
+            # count manual backups
+            if tag in ['manual', 'pre-restore']:
+                snap_by_type_retention_policy['manual']['found'] += count
+
+            # count update
+            if tag in ['update', 'sync-envs']:
+                snap_by_type_retention_policy['update']['found'] += count
+
+            if tag == 'SLA':
+                # this is where stuff gets complicated...
+                # we have multiple policies for SLA backups (hourly, daily, monthly, yearly, etc)
+                # but they are all tagged with SLA
+                # restic snapshots doesn't return data about retention, so we need to go through
+                # all SLA backups, determine which category they belong to and count them
+
+                # extract all SLA backups from snapshot_stats
+                sla_backups = {snap_id: snap_data for snap_id, snap_data in snapshot_stats.items()
+                               if snap_data['snapshot_tag'] == 'SLA'}
+
+                # requred for extraction of daily backups
+                hourly_start_timestamp = today.replace(hour=0, minute=0, second=0, microsecond=0).timestamp()
+                hourly_stop_timestamp = today.replace(hour=23, minute=59, second=59, microsecond=999999).timestamp()
+
+                backups_for_date = {}
+                backups_for_week = {}
+                backups_for_month = {}
+                backups_for_year = {}
+
+                current_date = datetime.now()
+
+                # we need to determine number of weeks covered by the weekly retention policy
+                weekly_start = (current_date - timedelta(weeks=self.retention_policy.get('weekly', 1))).timestamp()
+
+                # same thing for monthly retention policy
+                monthly_start = (current_date - timedelta(days=30 * self.retention_policy.get('monthly', 1))).timestamp()
+
+                # same thing for yearly retention policy
+                yearly_start = (current_date - timedelta(days=365 * self.retention_policy.get('yearly', 1))).timestamp()
+
+                for snap_id, snap_data in sla_backups.items():
+                    snapshot_timestamp = float(snap_data['snapshot_timestamp'])
+                    # count hourly backups
+
+                    if hourly_start_timestamp <= snapshot_timestamp <= hourly_stop_timestamp:
+                        snap_by_type_retention_policy['hourly']['found'] += 1
+
+                    # what are we doing here is basically we count the backups per day/week/month/year
+                    # and based on the number of keys in the dict, we can determine how many backups
+                    # for that period we have... and then, based on that, we can determine if the retention
+                    # policy is fulfilled or not
+                    backup_date = datetime.fromtimestamp(snapshot_timestamp).astimezone()
+
+                    # daily limit
+                    daily_start = backup_date.replace(hour=0, minute=0, second=0, microsecond=0).timestamp()
+                    daily_end = backup_date.replace(hour=23, minute=59, second=59, microsecond=999999).timestamp()
+
+                    day_key = backup_date.strftime('%Y%m%d')
+                    week_key = backup_date.strftime('%Y%W')
+                    month_key = backup_date.strftime('%Y%m')
+                    year_key = backup_date.strftime('%Y')
+
+                    # number of keys provides us with the info about daily backups
+                    # while number of values for the day provides us with the info about
+                    # hourly backups. this also provides us with a mechanism to extract
+                    # the latest backup for each day
+                    if daily_start <= snapshot_timestamp <= daily_end:
+                        if day_key not in backups_for_date:
+                            backups_for_date[day_key] = 1
+                        else:
+                            backups_for_date[day_key] += 1
+
+                    if yearly_start <= snapshot_timestamp <= current_date.timestamp():
+                        if year_key not in backups_for_year:
+                            backups_for_year[year_key] = 1
+                        else:
+                            backups_for_year[year_key] += 1
+
+                    # helps us count the weekly backups (number of keys)
+                    # and number of daily backups (values of the keys)
+                    if weekly_start <= snapshot_timestamp <= current_date.timestamp():
+                        if week_key not in backups_for_date:
+                            backups_for_week[week_key] = 1
+                        else:
+                            backups_for_week[week_key] += 1
+
+                    if monthly_start <= snapshot_timestamp <= current_date.timestamp():
+                        if month_key not in backups_for_date:
+                            backups_for_month[month_key] = 1
+                        else:
+                            backups_for_month[month_key] += 1
+
+                snap_by_type_retention_policy['daily']['found'] = len(backups_for_date.keys())
+                snap_by_type_retention_policy['weekly']['found'] = len(backups_for_week.keys())
+                snap_by_type_retention_policy['monthly']['found'] = len(backups_for_month.keys())
+                snap_by_type_retention_policy['yearly']['found'] = len(backups_for_year.keys())
+
         latest_snapshots_dup = self.get_snapshots(storagebox, True)
         latest_snapshots = {}
         for snap in latest_snapshots_dup:
@@ -319,22 +511,23 @@ class ResticCollector(object):
                 }
             else:
                 stats = self.get_stats(storagebox=storagebox, snapshot_id=snap["id"])
-                clients.append(
-                    {
-                        "storagebox": storagebox if storagebox else 'local',
-                        "repository": self.repository_name,
-                        "hostname": snap["hostname"],
-                        "username": snap["username"],
-                        "version": snap["program_version"] if "program_version" in snap else "",
-                        "snapshot_hash": snap["hash"],
-                        "snapshot_tag": snap["tags"][0] if "tags" in snap else "",
-                        "snapshot_paths": ",".join(snap["paths"]) if self.include_paths else "",
-                        "timestamp": snap["timestamp"],
-                        "size_total": stats["total_size"],
-                        "files_total": stats["total_file_count"],
-                        "snapshots_total": snap_total_counter[snap["hash"]],
-                    }
-                )
+
+            clients.append(
+                {
+                    "storagebox": storagebox if storagebox else 'local',
+                    "repository": self.repository_name,
+                    "hostname": snap["hostname"],
+                    "username": snap["username"],
+                    "version": snap["program_version"] if "program_version" in snap else "",
+                    "snapshot_hash": snap["hash"],
+                    "snapshot_tag": snap["tags"][0] if "tags" in snap else "",
+                    "snapshot_paths": ",".join(snap["paths"]) if self.include_paths else "",
+                    "timestamp": snap["timestamp"],
+                    "size_total": stats["total_size"],
+                    "files_total": stats["total_file_count"],
+                    "snapshots_total": snap_total_counter[snap["hash"]],
+                }
+            )
 
         # todo: fix the commented code when the bug is fixed in restic
         #  https://github.com/restic/restic/issues/2126
@@ -354,6 +547,7 @@ class ResticCollector(object):
 
         metrics = {
             "snapshots_by_type": snap_by_type_total_counter,
+            "snapshots_by_policy": snap_by_type_retention_policy,
             "snapshots_stats": snapshot_stats,
             "check_success": check_success,
             "locks_total": locks_total,
@@ -539,12 +733,23 @@ if __name__ == "__main__":
 
     # restic config
     filter_hosts = os.environ.get('RESTIC_FILTER_HOSTS', None)
-    retention_policy = os.environ.get('RESTIC_RETENTION_POLICY', None)
+    retention_policy_string = os.environ.get('RESTIC_RETENTION_POLICY', None)
+    backup_times_string = os.environ.get('BACKUP_TIMES', None)
+
+    # retention policy should be sent as a POLICY=VALUE comma separated list
+    # so if we have it, we need to split it and create a dict
+    retention_policy = {}
+    if retention_policy_string:
+        for policy in retention_policy_string.split(','):
+            policy_name, policy_value = policy.split('=')
+            retention_policy[policy_name] = int(policy_value)
+
+    backup_times = [3]
+    if backup_times_string:
+        backup_times = [int(x) for x in backup_times_string.split(',')]
 
     # repository info
     repository_name = os.environ.get('RESTIC_REPOSITORY_NAME', 'default')
-
-    # TODO: counts based on restic_retention_policy
 
     try:
         collector = ResticCollector(
@@ -554,8 +759,9 @@ if __name__ == "__main__":
             # rclone and restic params
             rclone_program=rclone_program,
             filter_hosts=filter_hosts,
-            restic_retention_policy=retention_policy,
+            retention_policy=retention_policy,
             repository_name=repository_name,
+            backup_times=backup_times,
             # exporter params
             exit_on_error=exporter_exit_on_error,
             disable_checks=exporter_disable_check,
