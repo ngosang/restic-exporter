4b2009e7de0fff5291805bc40ee2e8219d924200
DengelFred <github@dengelfred.de>
2026-02-06 00:05:49 +0100

    fix: silence the apscheduler logger, fix a pytest errors

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -646,9 +646,9 @@ def main(refresh_loop: bool = True) -> None:
         start_http_server(exporter_port, exporter_address)
         # noinspection HttpUrlsUsage
         logging.info("Serving at http://%s:%d", exporter_address, exporter_port)
-        logging.info("Refreshing stats every %d seconds", exporter_refresh_interval)
+        logging.debug("Refreshing stats every %d seconds", exporter_refresh_interval)
         scheduler.add_job(func=collector.refresh, trigger='interval', seconds=exporter_refresh_interval)
-        while True:
+        while refresh_loop:
             time.sleep(1)
 
     except (KeyboardInterrupt, SystemExit):
